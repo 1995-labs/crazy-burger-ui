3a05d88beae490de15dca5f9ab9efbb1a65a90b7
Marcus Manuh <marcusmanuh1@gmail.com>
2023-05-07 00:25:21 +0000

    add dynamic gcp locaiton

--- firebase.ts
+++ firebase.ts
@@ -16,21 +16,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
 };
 
-let app: firebase.app.App;
-
-if (typeof window !== "undefined") {
-  // firebase.initializeApp(firebaseConfig);
-  if (firebase.apps.length > 0) {
-    app = firebase.app();
-  } else {
-    app = firebase.initializeApp(firebaseConfig);
-  }
-  // firebase.performance();
-}
-// const appCheck = typeof window !== "undefined" && firebase.appCheck();
-// self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
-// typeof window !== "undefined" &&
-//   appCheck.activate(process.env.NEXT_PUBLIC_APP_CHECK_KEY, true);
+typeof window !== "undefined" && firebase.initializeApp(firebaseConfig);
 
 export const auth = typeof window !== "undefined" && firebase.app().auth();
 // auth.useEmulator("http://localhost:9099");
@@ -38,10 +24,8 @@ export const firestore =
   typeof window !== "undefined" && firebase.app().firestore();
 // firestore.useEmulator('localhost', 8080);
 export const functions =
-  typeof window !== "undefined" && firebase.app().functions();
+  typeof window !== "undefined" &&
+  firebase.app().functions(process.env.NEXT_PUBLIC_GCP_LOCATION);
 // functions.useEmulator("localhost", 5001);
-// export const messaging = app.messaging();
 
 export const timestamp = firebase.firestore.FieldValue.serverTimestamp;
-
-export default app;
